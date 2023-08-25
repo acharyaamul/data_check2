c940c3c8b6929752ca02c64f58451c21408a4cde
acharyaamul <acharya.amulkumar@gmail.com>
2023-08-25 19:44:37 +0545

    first commit

---
+++ data_check_25august2023.R
@@ -0,0 +1,3 @@
+#### Check the data of CEO and two times data of NFI####
+ceoNFI<- read.csv("ceo-NFI-ART-TREES-Data-Collection-2012-2021-sample-data-2023-08-25.csv",head=T)
+nfi<-read.csv("Leaf_NFIdata_11august2023.csv",header=T)
